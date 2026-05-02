9deb64775443551b047fa5d022eb473c5319421e
Philipp Mueller <philipp.mueller@rheinschafe.de>
2026-05-02 17:47:19 +0200

    task: layout stuff

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,16 +1,54 @@
 import { AppSidebar } from "@/components/custom/app-sidebar";
+import { LayersPlus, Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbPage, BreadcrumbList, BreadcrumbLink, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 export default function Dashboard({ ...props }) {
     return (
         <SidebarProvider>
-            <AppSidebar headline="T3Lingu" />
+            <AppSidebar />
             <SidebarInset>
                 <header className="flex h-12 items-center gap-2 px-4 border-b">
                     <SidebarTrigger />
                 </header>
                 <main>
-
+                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                                Build Your Application
+                            </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                    </header>
+                    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+                        <div className="aspect-video rounded-xl bg-emerald-700/30 grid content-center">
+                            <div className="grid auto-rows-fr">
+                                <LayersPlus />
+                                <Button className="inline-flex">
+                                    <Plus />
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="aspect-video rounded-xl bg-emerald-700/30" />
+                        <div className="aspect-video rounded-xl bg-emerald-700/30" />
+                        <div className="aspect-video rounded-xl bg-emerald-700/30" />
+                    </div>
+                    <div className="min-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+                    </div>
+                    <div className="flex justify-between">
+                        {props.headline}
+                    </div>
                 </main>
             </SidebarInset>
         </SidebarProvider>
--- resources/js/components/custom/app-sidebar.jsx
+++ resources/js/components/custom/app-sidebar.jsx
@@ -1,26 +1,30 @@
+import { Earth } from "lucide-react";
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
     SidebarHeader,
+    SidebarMenuBadge
 } from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
 
 export function AppSidebar({...props}) {
 
     return (
-        <Sidebar>
+        <Sidebar collapsible="icon">
             <SidebarHeader>
+                <div className="flex justify-center">
+                    <Earth className="size-12 stroke-emerald-700 text-emerald-700" />
+                </div>
             </SidebarHeader>
             <SidebarContent>
-                <div className="flex justify-between">
-                    {props.headline}
-                </div>
                 <SidebarGroup />
                     {props.children}
-                <SidebarGroup />
             </SidebarContent>
             <SidebarFooter />
+            <SidebarMenuBadge />
+            <NavUser className="self-end justify-end" />
         </Sidebar>
     );
 }
